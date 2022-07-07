cf34d0540c874e16adffee1792e771f440c6e331
Sama872 <107773332+Sama872@users.noreply.github.com>
2022-07-06 19:37:45 -0700

    Add files via upload

---
+++ project4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Students;
+
+/**
+ *
+ * @author hp
+ */
+public class project4 {
+    
+    int age;
+    String name;
+    project4(){
+        age=5;
+        name="";
+    }
+    project4 (int age ,String name){
+        this.age=age;
+        this.name=name;
+    }
+    
+}
+    
+
